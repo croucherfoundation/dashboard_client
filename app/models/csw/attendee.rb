3e33444fdc863d3f256d263b8ae4bc4e16ea8475
Thi Ha <thiha.dve@gmail.com>
2024-10-23 16:41:23 +0900

    account sync

--- app/models/csw/attendee.rb
+++ app/models/csw/attendee.rb
@@ -1,5 +1,16 @@
 class Csw::Attendee < ActiveResource::Base
-  include DashboardFormatApiResponse
   include DashboardActiveResourceConfig
+  self.site                   = ENV['DASHBOARD_API_URL']
+  self.prefix                 = '/api/csw/admin/'
+  self.element_name           = "attendee"
+  self.format                 = DashboardFormatApiResponse
+  self.include_format_in_path = false
 
+  def self.find_by_email(email)
+    response = get(:search_by_email, { email: email })
+    return nil if response.nil? || response.empty?
+    new(response)
+  rescue ActiveResource::ResourceNotFound
+    nil
+  end
 end
