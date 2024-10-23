17294a011dc7dfe580e03ecbcb699321241ed87e
Thi Ha <thiha.dve@gmail.com>
2024-10-23 19:06:24 +0900

    accout sync

--- app/models/csw/attendee.rb
+++ app/models/csw/attendee.rb
@@ -9,7 +9,15 @@ class Csw::Attendee < ActiveResource::Base
   def self.find_by_email(email)
     response = get(:search_by_email, { email: email })
     return nil if response.nil? || response.empty?
-    new(response)
+    instantiate_record(response)
+  rescue ActiveResource::ResourceNotFound
+    nil
+  end
+
+  def self.confirm(confirmation_token)
+    response = get(:confirm, { confirmation_token: confirmation_token })
+    return nil if response.nil? || response.empty?
+    instantiate_record(response)
   rescue ActiveResource::ResourceNotFound
     nil
   end
